cc9be82d7172e778657c25db28ea9d6348256667
Gosttlt <55979136+Gosttlt@users.noreply.github.com>
2020-06-23 17:16:48 +0400

    added typing 'sidebar-reducer'

--- src/Redux/sidebar-reducer.ts
+++ src/Redux/sidebar-reducer.ts
@@ -1,12 +1,21 @@
+
+type userOnlineType = {
+    img: string 
+    user_name: string
+}
+
+
 let initialState = {
     userOnline: [
         { img: 'https://avatars.mds.yandex.net/get-pdb/1025945/86f10e6d-eab5-4572-8c07-c672b854d2a5/s1200?webp=false', user_name: 'Ivan' },
         { img: 'https://avatars.mds.yandex.net/get-pdb/1667260/3ec2154a-cd3f-4c24-b13b-6c60951dd0b0/s1200', user_name: 'Vasilii' },
         { img: 'https://i.livelib.ru/auface/471251/o/ab9f/Yurij_Belous.jpg', user_name: 'Petr' },
-    ],
+    ] as Array<userOnlineType>,
 };
 
-const sidebarReducer = (state = initialState, action) => {
+type initialStateType = typeof initialState; 
+
+const sidebarReducer = (state = initialState, action: any): initialStateType => {
 return state
 }
 export default sidebarReducer;
